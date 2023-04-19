c23df26562b76b9ae3dfb41599fdb5c799a3df81
kite <huynhanhkiet179@gmail.com>
2023-04-19 10:18:48 +0700

    config for deploying to heroku

--- src/main/java/com/vti/group1/shopapi/controller/HelloWorldController.java
+++ src/main/java/com/vti/group1/shopapi/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/api/hello")
+    @GetMapping("/")
     public String hello() {
         return "Hello World";
     }
