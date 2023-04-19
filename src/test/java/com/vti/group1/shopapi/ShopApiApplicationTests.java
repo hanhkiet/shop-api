c377f1515034fe674c4db9294d4b6d2a4654e7aa
kite <huynhanhkiet179@gmail.com>
2023-04-19 10:11:41 +0700

    first commit

---
+++ src/test/java/com/vti/group1/shopapi/ShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.group1.shopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
