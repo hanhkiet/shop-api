00bb231110b00b0414f2d12bf11c5ced733101e7
hanhkiet <huynhanhkiet179@gmail.com>
2023-04-24 15:02:40 +0700

    config for deploying to google cloud

--- src/main/java/com/vti/group1/shopapi/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vti.group1.shopapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vti.group1.shopapi.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
