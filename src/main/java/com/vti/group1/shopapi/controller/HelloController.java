9b65dbda9e7426028a1014a80b68c796917e6dcc
hanhkiet <huynhanhkiet179@gmail.com>
2023-04-26 09:54:07 +0700

    update project for connect to cloudsql

---
+++ src/main/java/com/vti/group1/shopapi/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.vti.group1.shopapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "VTI Shop API";
+    }
+}
