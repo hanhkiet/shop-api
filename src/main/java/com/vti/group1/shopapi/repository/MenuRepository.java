6bd99ecee67b5d54a522feb7ad032ba2e31e7e76
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-05-14 16:15:52 +0700

    [Feature] SHOP-83: write API menus & megamenus

--- src/main/java/com/vti/group1/shopapi/repository/MenuRepository.java
+++ src/main/java/com/vti/group1/shopapi/repository/MenuRepository.java
@@ -1,2 +1,9 @@
-package com.vti.group1.shopapi.repository;public interface MenuRepository {
+package com.vti.group1.shopapi.repository;
+
+import com.vti.group1.shopapi.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+    Optional<Menu> findById(Long id);
 }
