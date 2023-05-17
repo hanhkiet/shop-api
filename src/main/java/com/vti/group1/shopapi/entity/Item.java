ef9dca36af96b5454384f7809515fa68bd0d9c07
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-05-17 11:55:13 +0700

    Create Item entity & StringListConverter

--- src/main/java/com/vti/group1/shopapi/entity/Item.java
+++ src/main/java/com/vti/group1/shopapi/entity/Item.java
@@ -1,2 +1,27 @@
-package com.vti.group1.shopapi.entity;public class Item {
+package com.vti.group1.shopapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "items")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "product_uuid")
+    private Product product;
+    private Size size;
+    private Color color;
+    private Integer quantity;
 }
