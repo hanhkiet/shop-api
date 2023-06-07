440d6681247e701c382d0ec55f46f948976fcff9
hanhkiet <huynhanhkiet179@gmail.com>
2023-06-07 11:08:03 +0700

    fix ProductRepository

--- src/main/java/com/vti/group1/shopapi/repository/ProductRepository.java
+++ src/main/java/com/vti/group1/shopapi/repository/ProductRepository.java
@@ -1,17 +1,13 @@
 package com.vti.group1.shopapi.repository;
 
+import com.vti.group1.shopapi.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-
-import com.vti.group1.shopapi.entity.Product;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
 import java.util.Optional;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.vti.group1.shopapi.entity.Product;
 
 public interface ProductRepository extends PagingAndSortingRepository<Product, String> {
 
@@ -19,4 +15,10 @@ public interface ProductRepository extends PagingAndSortingRepository<Product, S
 
     @Query("SELECT p FROM Product p WHERE p.name LIKE %?1%")
     Page<Product> searchProducts(String query, PageRequest of);
+
+    Product save(Product product);
+
+    void delete(Product product);
+
+    List<Product> findAll();
 }
